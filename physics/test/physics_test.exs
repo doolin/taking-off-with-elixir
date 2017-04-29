f24940e60e60934974d102b2654e191d75227a26
dave doolin <david.doolin@gmail.com>
2017-04-29 08:55:58 -0700

    first working example code with tests
    Having difficulty with the tone of the tutorial.
    
    Really disappointed the tutorial isn't leading off
    with tests.
    
    In any case, this is some initial code with a couple
    of tests implemented.

---
+++ physics/test/physics_test.exs
@@ -0,0 +1,18 @@
+defmodule PhysicsTest do
+  use ExUnit.Case
+  doctest Physics
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+  test 'escape velocity of earth is correct' do
+    assert Physics.Rocketry.escape_velocity(:earth) == 11.2
+  end
+
+  test 'escape velocity of planet X is correct' do
+    planet_x = %{mass: 4.0e22, radius: 6.21e6}
+    ev = planet_x |> Physics.Rocketry.escape_velocity
+    assert ev == 0.9
+  end
+end
