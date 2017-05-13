9e141e3b166c839ddce08dab9abf41369d98ee7a
dave doolin <david.doolin@gmail.com>
2017-05-13 16:31:44 -0700

    recursion: rocketry test examples

--- physics/lib/solar.ex
+++ physics/lib/solar.ex
@@ -12,4 +12,15 @@ defmodule Solar do
   def deadliest(flares) do
     Enum.map(flares, &(power(&1))) |> Enum.max
   end
+
+  def flare_power(list) do
+    total_flare_power(list, 0)
+  end
+
+  def total_flare_power([], total), do: total
+
+  def total_flare_power([%{classification: :M, scale: s} | tail], total) do
+    new_total = s * 10 * 0.92 + total
+    total_flare_power(tail, new_total)
+  end
 end
---
+++ physics/test/rocketry_test.exs
@@ -0,0 +1,20 @@
+defmodule RocketryTest do
+  use ExUnit.Case
+  import Physics.Rocketry
+
+  @tag :skip
+  test "orbital acceleration defaults to earth" do
+  end
+
+  @tag :skip
+  test "orbital term at 100km" do
+  end
+
+  @tag :skip
+  test "orbital acceleration for Jupiter at 100km" do
+  end
+
+  @tag :skip
+  test "orbital acceleration for Saturn at 6000km" do
+  end
+end
