9c37462c13cad1fc20dcfbfb4025362c54d1679d
dave doolin <david.doolin@gmail.com>
2017-05-24 07:53:50 -0700

    otp first part, postgres working
    This commit adds all the necessary whatnot to get the
    postgres data loading task working.
    
    The fog has finally rolled in. I'm going to lunch today
    with KK and MK from CP, maybe Theo if he's in. Should
    be fun to catch up.

--- physics/lib/physics.ex
+++ physics/lib/physics.ex
@@ -3,6 +3,18 @@ defmodule Physics do
   Documentation for Physics.
   """
 
-  alias Physics.Rocketry, as: Astrophysics
-  alias Physics.Laws
+  # alias Physics.Rocketry, as: Astrophysics
+  # alias Physics.Laws
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Physics.Repo, [])
+    ]
+
+    opts = [strategy: :one_for_one, mame: Physics.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
--- physics/mix.exs
+++ physics/mix.exs
@@ -15,7 +15,8 @@ defmodule Physics.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :timex, :postgrex, :ecto]]
+    [extra_applications: [:logger, :timex, :postgrex, :ecto],
+    mod: {Physics, []}]
   end
 
   # Dependencies can be Hex packages:
